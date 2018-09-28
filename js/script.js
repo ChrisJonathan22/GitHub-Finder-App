a092736addc4c8fb3c928086ba49ee47c2344dd0
Chris Jonathan <christophereko@hotmail.fr>
2018-09-28 20:50:20 +0100

    Github User Finder
    It's a basic app which let's you search for GitHub users.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+        $('#searchUser').on('keyup', function(e){
+                let username = e.target.value;
+
+
+                // Make request to Github
+                $.ajax({
+                        url: 'https://api.github.com/users/' + username,
+                        data: {
+                                client_id: 'd4e5565df611a4fc8bb2' ,
+                                client_secret: '398f82509532a5bfc666f64dcfc522656cde13e4'
+                        }
+                }).done(function(user){
+                        $('#profile').html(''
+
+                        <div class="panel panel-default">
+                                <div class="panel-heading">
+                                        <h3 class="panel-title">${user.name}</h3>
+                                </div>
+                                <div class="panel-body">
+                                Panel content
+                                </div>
+                        </div>
+
+
+                        '');
+                });
+        });
+});
